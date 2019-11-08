22ead081688307b3bff73d9c583e010b837b428c
邢雅杰 <470446087@qq.com>
2019-11-08 14:23:12 +0800

    国际化

---
+++ AutoLocalizable.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+
+def main():
+	# sys.argv[1] 后面改为传参数方式
+	filePath = '/Users/xyj/Private/思源/iOS/BaseProject'
+
+
+if __name__ == '__main__':
+	main()
+
