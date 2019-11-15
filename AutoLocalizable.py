ccdedb0be1e06683928900682719bed7af4fae2b
邢雅杰 <470446087@qq.com>
2019-11-15 15:00:12 +0800

    ibtool 生成strings文件

--- AutoLocalizable.py
+++ AutoLocalizable.py
@@ -4,6 +4,13 @@ import glob
 import os
 import sys
 
+TempStringsFile = 'temp.strings'
+
+
+def executeIbtoolShell(filePath, resultPath):
+    cmdString = 'ibtool ' + filePath + ' --generate-strings-file ' + resultPath
+    return os.system(cmdString) == 0
+
 
 # 生成文件名
 def extractFileName(file_path):
@@ -20,15 +27,51 @@ def extractFilePrefix(file_path):
     return prefix
 
 
+# 处理xib或sb文件，传入目标xib或sb文件path, 文件类型;
+def dealWithXibOrSbFiles(targetFilePath, fileTypeName):
+    baseStrIdx = targetFilePath.find('Base.lproj')
+    if baseStrIdx < 0:
+        return
+    parentFilePath = targetFilePath[0:(baseStrIdx - 1)]
+
+    filePathName = targetFilePath + '/*.' + fileTypeName
+
+    targetFilePaths = glob.glob(filePathName)
+    print('获得' + fileTypeName + '的具体文件')
+    print(targetFilePaths)
+    if len(targetFilePaths) == 0:
+        print('目录：' + parentFilePath + '下，没有storyboard文件')
+        return
+    resultFilePath = parentFilePath + '/' + '*.lproj/*.strings'
+    resultFile_list = glob.glob(resultFilePath)
+    tempFile_Path = parentFilePath + '/' + 'TempStoryboard.strings'
+    if len(targetFile_list) == 0:
+        print('目录：' + parentFilePath + '下，没有strings文件')
+        return
+    for filePath in targetFilePaths:
+        sourceprefix = extractFilePrefix(filePath)
+        sourcename = extractFileName(filePath)
+        print('-----')
+        print(targetFilePath)
+        print(sourceprefix)
+        print('目标文件名：' % sourcename)
+        print('-----')
+
+
 def findXibOrSbFilesIn(dir):
     resultPaths = []
     # 三个参数：1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
     for parent, dirnames, filenames in os.walk(dir):
         for filename in filenames:  # 输出文件信息
             if ('.xib' in filename) | ('.storyboard' in filename):
+                print(filename)
                 filePath = os.path.join(parent)
-                if filePath not in resultPaths:
-                    resultPaths.append(filePath)
+                print(parent)
+                print(filePath)
+                fullFilePath = filePath + '/' + filename
+                print(fullFilePath)
+                if fullFilePath not in resultPaths:
+                    resultPaths.append(fullFilePath)
     return resultPaths
 
 
@@ -42,26 +85,6 @@ def main():
         if baseStrIdx >= 0:
             sourceFilePathName = sourceFilePath + '/*.storyboard'
             upperFilePath = sourceFilePath[0:(baseStrIdx - 1)]
-            sourceFile_list = glob.glob(sourceFilePathName)
-            print('获得storyboard的具体文件')
-            print(sourceFile_list)
-            if len(sourceFile_list) == 0:
-                print('目录：' + upperFilePath + '下，没有storyboard文件')
-                return
-            targetFilePath = upperFilePath + '/' + '*.lproj/*.strings'
-            targetFile_list = glob.glob(targetFilePath)
-            tempFile_Path = upperFilePath + '/' + 'TempStoryboard.strings'
-            if len(targetFile_list) == 0:
-                print('目录：' + upperFilePath + '下，没有strings文件')
-                return
-            for sourcePath in sourceFile_list:
-                sourceprefix = extractFilePrefix(sourcePath)
-                sourcename = extractFileName(sourcePath)
-                print('-----')
-                print(sourcePath)
-                print(sourceprefix)
-                print('目标文件名：' % sourcename)
-                print('-----')
 
 
 if __name__ == '__main__':
