2760a2d2f168400c817f6736de7aeed879f209dd
邢雅杰 <470446087@qq.com>
2022-06-10 21:12:12 +0800

    文件中需本地化文件处理

--- AutoLocalizable.py
+++ AutoLocalizable.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import DealCodeFile
 import DealXibOrSbFile
 def main():
     # sys.argv[1] 后面改为传参数方式
     filePath = '/Users/xyj/Private/思源/iOS/BaseProject'
     DealXibOrSbFile.dealXibOrSbFilePath(filePath)
-    
+
+    DealCodeFile.dealCodeFilePath(filePath)
+
 
 if __name__ == '__main__':
     main()
