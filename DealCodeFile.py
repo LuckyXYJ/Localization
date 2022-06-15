860d66513c557279eb579814143a0a129f3fa184
邢雅杰 <470446087@qq.com>
2022-06-15 21:12:12 +0800

    抽取合并strings文件方法

--- DealCodeFile.py
+++ DealCodeFile.py
@@ -1,109 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import codecs
-import glob
 import operator
 import os
-import re
-import time
-import chardet
 
-TempStringsFile = 'temp.strings'
-StringRegex = r'["](.*?)["]'
+import MergeStringsFiles
 
-ResultParamRegex = r'["](.*?)["](\s*)=(\s*)["](.*?)["];'
-CommentParamRegex = r'/(.*?)/'
-
-def getTxtWithString(string):
-    if string is None:
-        return ''
-    else:
-        resultStringType = chardet.detect(string)
-        resultStringText = string.decode(resultStringType['encoding'])
-        return resultStringText
-
-
-def constructCommentRegex(str):
-    return CommentParamRegex + '\n' + str
-
-def getCommentOfString(string_txt, suffix_string):
-    commentRegex = constructCommentRegex(suffix_string)
-    commentMatch = re.search(commentRegex, string_txt)
-    commentString = ''
-    if commentMatch:
-        match = re.search(CommentParamRegex, commentMatch.group(0))
-        if match:
-            commentString = match.group(0)
-    return commentString
-
-
-def dealWithStringsFile(orgFilePath, tempFilePath):
-    print('原strings文件        ===', orgFilePath)
-    print('新生成strings文件     ===', tempFilePath)
-
-    with codecs.open(tempFilePath, 'rb') as temp_file:
-        tempString = temp_file.read()
-        tempStringText = getTxtWithString(tempString)
-
-        resultDict = {}
-        commentDict = {}
-        for iterator in re.finditer(ResultParamRegex, tempStringText):
-            linestr = iterator.group(0)
-            commentString = getCommentOfString(tempStringText, linestr)
-            linematchs = re.findall(StringRegex, linestr)
-            if len(linematchs) == 2:
-                leftvalue = linematchs[0]
-                rightvalue = linematchs[1]
-                resultDict[leftvalue] = rightvalue
-                commentDict[leftvalue] = commentString
-            print('新字典 ----%s' % resultDict)
-            print('不用的字典 ----%s' % commentDict)
-
-    with codecs.open(orgFilePath, 'rb') as org_file:
-        orgFileString = org_file.read()
-        orgFileStringText = getTxtWithString(orgFileString)
-
-        # print("读取的源文件内容：", orgFileStringText)
-        originalDict = {}
-        for iterator in re.finditer(ResultParamRegex, orgFileStringText):
-            linestr = iterator.group(0)
-            linematchs = re.findall(StringRegex, linestr)
-            if len(linematchs) == 2:
-                leftvalue = linematchs[0]
-                rightvalue = linematchs[1]
-                originalDict[leftvalue] = rightvalue
-
-    for key in originalDict:
-        if (key not in resultDict):
-            print('=======', key)
-            keystr = '"%s"' % key
-            print(keystr)
-            replacestr = '//' + keystr
-            match = re.search(replacestr, orgFileStringText)
-            if match is None:
-                orgFileStringText = orgFileStringText.replace(keystr, replacestr)
-
-    addHeader = True
-    for key in resultDict:
-        values = (key, resultDict[key])
-        newline = ''
-        if addHeader == True:
-            timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-            newline = '\n/// ----------------------------------------\n'
-            newline += '/// AutoLocalizable            ' + timestamp + '\n'
-            newline += '/// ----------------------------------------\n'
-            addHeader = False
-        newline += '\n' + commentDict[key]
-        newline += '\n"%s" = "%s";\n' % values
-        orgFileStringText += newline
-    with codecs.open(orgFilePath, 'wb') as resultFile:
-        resultFile.write(orgFileStringText.encode('UTF-8'))
-
-
-#   读取source文件内容准备对比
-#   对比文件添加或者删除内容
-#   移除temp文件
 
 def generateCodeFiles(projectPath):
     print("代码国际化")
@@ -132,7 +34,7 @@ def generateCodeLocalizableFile(sourcePaths, projectPath):
     nameString = 'Localizable.strings'
     tempPathFile = tempPath + '/' + nameString
     for sourcePath in sourcePaths:
-        dealWithStringsFile(sourcePath, tempPathFile)
+        MergeStringsFiles.dealWithStringsFile(sourcePath, tempPathFile)
     os.remove(tempPathFile)
 
 
