96e0c3892061c75daa227fc7efda8bff5f847f01
邢雅杰 <470446087@qq.com>
2022-06-23 21:12:12 +0800

    注释

--- AutoLocalizable.py
+++ AutoLocalizable.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+import sys
+
 import DealCodeFile
 import DealXibOrSbFile
+
+
 def main():
     # sys.argv[1] 后面改为传参数方式
-    filePath = '/Users/xyj/Private/思源/iOS/BaseProject'
+    if len(sys.argv) == 1:
+        filePath = os.path.dirname(os.getcwd())
+    else:
+        filePath = sys.argv[1]
     DealXibOrSbFile.dealXibOrSbFilePath(filePath)
-
     DealCodeFile.dealCodeFilePath(filePath)
 
 
